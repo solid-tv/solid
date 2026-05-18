377680f9d5b03fd4721a048eef7b4eb549b213d7
Mirko Pecora <mi.pecora@reply.it>
2026-05-18 16:58:49 +0200

    fix(config): fix tests

--- src/core/config.ts
+++ src/core/config.ts
@@ -24,10 +24,14 @@ import {
 export const isDev = !!(import.meta.env && import.meta.env.DEV);
 
 /** Whether the DOM renderer is used instead of `@solidtv/renderer` */
-export const DOM_RENDERING = SOLIDTV_DOM_RENDERING === true;
+export const DOM_RENDERING =
+  typeof SOLIDTV_DOM_RENDERING !== 'undefined' &&
+  SOLIDTV_DOM_RENDERING === true;
 
 /** Whether element shaders are enabled */
-export const SHADERS_ENABLED = SOLIDTV_DISABLE_SHADERS !== true;
+export const SHADERS_ENABLED =
+  typeof SOLIDTV_DISABLE_SHADERS === 'undefined' ||
+  SOLIDTV_DISABLE_SHADERS !== true;
 
 /**
   RUNTIME LIGHTNING CONFIGURATION \
